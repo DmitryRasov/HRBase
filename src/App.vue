1a97528f724aac4b2267f3cdcc8a137a1adda636
DmitryRasov <letucepray@yandex.ru>
2023-07-07 14:20:48 +0300

    start ui

--- src/App.vue
+++ src/App.vue
@@ -23,7 +23,10 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+
 body {
+  font-family: 'Roboto', sans-serif;
   background-color: #f0f0ef;
 }
 </style>
